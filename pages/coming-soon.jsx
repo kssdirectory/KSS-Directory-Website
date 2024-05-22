f9a0f658880dfe396e504c51c9eaee777ff700a9
HynixCJR <134127116+HynixCJR@users.noreply.github.com>
2024-05-22 00:47:46 -0400

    added testing page for future club directory

--- pages/coming-soon.jsx
+++ pages/coming-soon.jsx
@@ -9,9 +9,9 @@ function ComingSoonPage() {
             <Head>
                 <title>Coming soon!</title>
 
-                <meta name="description" content="KSS Directory is a student-run resource repository for Kingston Secondary School, where you can easily access a complete announcement archive, alongside a plethora of other student resources! Made by Matthew Kong." />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" sizes="76x76" href="static/compassLogo.ico" />
+                    <meta name="description" content="KSS Directory is a student-run resource repository for Kingston Secondary School, where you can easily access a complete announcement archive, alongside a plethora of other student resources! Made by Matthew Kong." />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" sizes="76x76" href="static/compassLogo.ico" />
             </Head>
             <Link href = "/" style = {{"margin": "0px"}}>
                 <div style = {{height: "550px", padding: "calc(100vw/10)", background: "#072136", margin: "25px", "border-radius": "32px", animation: "0.5s cubic-bezier(0.02, 0.38, 0.22, 0.98) 0s 1 slideInTop"}}>
---
+++ pages/test-page.jsx
@@ -0,0 +1,117 @@
+import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import Link from "next/link";
+import React from 'react';
+import main from '../styles/test-page/main.module.css'
+
+
+
+const TestPage = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    function scrollingElement(scrollPos) {
+      const mainColour = "#BA7312";
+      const heroContent = <div className={main.heroSection}>
+        <img
+        src="https://lh7-us.googleusercontent.com/MxFWmpW1pg3pPEliCc2awEOdFlloiwNHXNlYeKEfmJPP-LhLLPFw8GaTCRdVPjioN6LmOQtpPvfPVgEf4xSMOC2_y67ucuLxipwMhkNNxBklzxiioqMJPwy07cMzX-Vclf-PsyvHkJfdDFN-P_z8RsycZOALYytY"
+        style={{width:500,height:500}}
+        ></img>
+        hi
+      </div>;
+      if (scrollPos < 100) {
+
+        return <div className={main.tilesFlexBox} style={{ alignItem: "center", justifyContent: "center" }}>
+          <div style={{
+            backgroundColor: mainColour,
+            width: window.innerWidth - scrollPos * 2,
+            height: window.innerHeight - scrollPos * 2,
+            borderRadius: scrollPos / 2,
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}>
+            {heroContent}
+          </div>
+        </div>
+      } else if (scrollPos < window.innerWidth / 3.4) {
+        console.log(scrollPos)
+        return <div className={main.tilesFlexBox}>
+          <div style={{
+            backgroundColor: mainColour,
+            width: window.innerWidth - scrollPos * 2,
+            height: window.innerHeight - 200,
+            borderRadius: 50,
+            position: "fixed",
+            top: 100,
+            left: 100
+          }}></div>
+
+          <div style = {{
+            width: window.innerWidth / 3.4,
+            height: window.innerHeight - 200,
+            backgroundColor: "red",
+            position: "fixed",
+            marginLeft: window.innerWidth - scrollPos * 2 + 150,
+            position: "fixed",
+            top: 100
+            }}>
+
+          </div>
+          <div style = {{width: 50, height: "100vh", backgroundColor: "red", position: "fixed", marginLeft: 100}}>
+
+          </div> 
+        </div>
+      } else if (scrollPos > window.innerWidth / 3.4) {
+        return <div className={main.tilesFlexBox} style={{ alignItem: "center", justifyContent: "center" }}>
+          <div style={{
+            backgroundColor: mainColour,
+            width: window.innerWidth - window.innerWidth / 3.4 * 2,
+            height: window.innerHeight - 200,
+            borderRadius: 50,
+            position: "fixed",
+            top: 100,
+            left: 100
+          }}></div>
+        </div>
+      }
+    }
+    const handleScroll = () => {
+      setScrollY([window.scrollY, scrollingElement(window.scrollY)]);
+    };
+
+    // just trigger this so that the initial state 
+    // is updated as soon as the component is mounted
+    // related: https://stackoverflow.com/a/63408216
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+
+  }, []);
+
+
+  return (
+    <>
+      <Head>
+        <title>Test Page!</title>
+
+        <meta name="description" content="KSS Directory is a student-run resource repository for Kingston Secondary School, where you can easily access a complete announcement archive, alongside a plethora of other student resources! Made by Matthew Kong." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" sizes="76x76" href="static/compassLogo.ico" />
+      </Head>
+      <div style={{ height: 8000 }}> {/* just added to make scrollbar available */}
+        <div style={{ position: "fixed", top: 0 }}>
+          {scrollY[0]}
+        </div>
+        {scrollY[1]}
+      </div>
+    </>
+  );
+};
+
+export default TestPage;
