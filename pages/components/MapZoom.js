934917accbdacf052ed6f9ad88f168bac73e2033
HynixCJR <134127116+HynixCJR@users.noreply.github.com>
2024-05-28 14:23:39 -0400

    updated the map zoom

--- pages/components/MapZoom.js
+++ pages/components/MapZoom.js
@@ -1,45 +1,23 @@
-import React, { useRef, useState } from 'react';
-import { useGesture } from '@use-gesture/react';
-import { useSpring, animated } from '@react-spring/web';
+import React, { useCallback, useRef } from "react";
+import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
 
-const MapSvgZoom = ({ src }) => {
-  const svgRef = useRef();
-  const [{ x, y, scale }, api] = useSpring(() => ({ x: 0, y: 0, scale: 1 }));
+const MapSvgZoom = ( {src} ) => {
+  const imgRef = useRef();
+  const onUpdate = useCallback(({ x, y, scale }) => {
+    const { current: img } = imgRef;
 
-  const bind = useGesture(
-    {
-      onDrag: ({ offset: [x, y] }) => {
-        console.log(`Dragging to x: ${x}, y: ${y}`);
-        api.start({ x, y })
-      },
-      onPinch: ({ offset: [scale] }) => {
-        console.log(`Pinching with scale: ${scale}`);
-        api.start({ scale: 1 + scale / 100 })
-      }
-    },
-    {
-      drag: { from: () => [x.get(), y.get()] },
-      pinch: { scaleBounds: { min: 0.5, max: 4 }, from: () => [scale.get() - 1] },
+    if (img) {
+      let scaleMod = scale * 5;
+      const value = make3dTransformValue({ x, y, scale});
+
+      img.style.setProperty("transform", value);
     }
-  );
+  }, []);
 
   return (
-    <div
-      {...bind()}
-      style={{ touchAction: 'none', overflow: 'hidden', width: '100%', height: '100%' }}
-    >
-      <animated.svg
-        ref={svgRef}
-        style={{
-          width: '100%',
-          height: '100%',
-          transformOrigin: 'center center',
-          transform: scale.to(s => `scale(${s})`) + x.to((x) => `translate(${x}px)`) + y.to((y) => `translate(${y}px)`),
-        }}
-      >
-        <image href={src} x="400" y="0" width="800" height="600" />
-      </animated.svg>
-    </div>
+    <QuickPinchZoom onUpdate={onUpdate}>
+      <img ref={imgRef} src={src} />
+    </QuickPinchZoom>
   );
 };
 export default MapSvgZoom;
