f8f871dc67d8ae5e64f7123f21308af62e6ba32b
HynixCJR <134127116+HynixCJR@users.noreply.github.com>
2024-05-29 01:20:26 -0400

    updated map (properly zoomable and pannable)
    the height is set incorrectly, so the bottom part of the map may not be visible

--- pages/components/MapZoom.js
+++ pages/components/MapZoom.js
@@ -1,23 +1,51 @@
 import React, { useCallback, useRef } from "react";
 import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
+import { ReactSVG } from "react-svg";
 
-const MapSvgZoom = ( {src} ) => {
-  const imgRef = useRef();
+const MapSvgZoom = ({ src }) => {
+  const containerRef = useRef();
+
   const onUpdate = useCallback(({ x, y, scale }) => {
-    const { current: img } = imgRef;
+    const { current: container } = containerRef;
 
-    if (img) {
-      let scaleMod = scale * 5;
-      const value = make3dTransformValue({ x, y, scale});
-
-      img.style.setProperty("transform", value);
+    if (container) {
+      const value = make3dTransformValue({ x, y, scale });
+      container.style.setProperty("transform", value);
+      container.style.setProperty("transform-origin", "0 0");
     }
   }, []);
 
   return (
-    <QuickPinchZoom onUpdate={onUpdate}>
-      <img ref={imgRef} src={src} />
+    <QuickPinchZoom
+      onUpdate={onUpdate}
+      inertia={true}
+      enforceBoundsDuringZoom={false}
+      wheelScaleFactor={1000}
+      draggableUnZoomed={false}
+      verticalPadding={15}
+      horizontalPadding={15}
+      doubleTapToggleZoom={true}
+    >
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+
+        }}
+      >
+        <div
+          ref={containerRef}
+          style={{
+            width: "100%",
+            height: "100%",
+            transformOrigin: "0 0",
+          }}
+        >
+          <ReactSVG src={src} style={{ width: "100%", height: "100%" }} />
+        </div>
+      </div>
     </QuickPinchZoom>
   );
 };
+
 export default MapSvgZoom;
--- pages/map-page.jsx
+++ pages/map-page.jsx
@@ -3,8 +3,8 @@ import MapSvgZoom from './components/MapZoom';
 
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <MapSvgZoom src="/svg_assets/map_page/floor_1.svg" />
+    <div style={{ width: '100vw', height: '100vh', backgroundColor:"var(--b0BG)"}}>
+      <MapSvgZoom src="/svg_assets/map_page/floor_1_and_ground.svg" style={{ width: '100vw', height: '100vh'}}/>
     </div>
   );
 }
