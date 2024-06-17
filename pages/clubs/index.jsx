a1fc6581d9771b64a12b3b38b8baa2a791510a40
HynixCJR <134127116+HynixCJR@users.noreply.github.com>
2024-06-17 01:12:27 -0400

    added NavBar to landing page of club repo

--- pages/clubs/index.jsx
+++ pages/clubs/index.jsx
@@ -1,4 +1,11 @@
 import Link from 'next/link'
+import Head from 'next/head';
+import main from '../../styles/club_directory/landing_page/main.module.css';
+import NavBar from "../../components/navBar";
+
+
+
+
 const webServerURL = "https://a23b-35-227-86-218.ngrok-free.app"
 
 
@@ -18,15 +25,36 @@ export default function clubsPage({ all_club_pages }) {
         }
     }
     return (
-        <div>
-            <h1>Hello</h1>
-            {listed_pages.map(listed_pages => (
-                <Link href={'/clubs/' + listed_pages.Content.Metadata.URL} key={listed_pages.Content.Metadata.URL}>
-                    <div>
-                        <h3>{listed_pages.Content.Metadata.Club_Name}</h3>
-                    </div>
-                </Link>
-            ))}
-        </div>
+        <>
+            <Head>
+                <title>KSS Directory Club Repository</title>
+
+                {/* Setting the favicon of the site to the KSS Directory logo */}
+                {/* Not the individual logo of each club because that'd be a PITA, especially if they didn't use a perfectly square logo...*/}
+                <link rel="icon" sizes="76x76" href="../static/compassLogo.ico" />
+            </Head>
+            <main id={main.bg}>
+                <NavBar
+                    extra_additions={(
+                        <div id={main.header_path_div}>
+                            <p id={main.header_path_link} href="/clubs">CLUB REPOSITORY</p>
+                        </div>
+                    )}
+                />
+
+                <div style={{width:"100vw"}}>
+                    <h1>Hello</h1>
+                    {listed_pages.map(listed_pages => (
+                        <Link href={'/clubs/' + listed_pages.Content.Metadata.URL} key={listed_pages.Content.Metadata.URL}>
+                            <div>
+                                <h3>{listed_pages.Content.Metadata.Club_Name}</h3>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </main>
+            
+        </>
+        
     )
 }
