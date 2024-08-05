456e76577488329af52559736ec82b53060ca375
Toast-Bucket <71938947+Kabinet0@users.noreply.github.com>
2024-08-05 14:43:48 -0400

    Analytics Take Two

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
       </Head>
       <Component {...pageProps} />
+      <Analytics/>
     </>
   )
 }
