ce0b62e11dee7a355db89f7eb3489099d7a1d78d
Tyishen <ethant.ca@gmail.com>
2024-09-12 23:04:09 -0400

    man i don't even know the navbar is dead

--- components/directory/DirectoryNavBar.jsx
+++ components/directory/DirectoryNavBar.jsx
@@ -9,11 +9,19 @@ const NavBar = ({ extra_additions, text_color = "#072136", center_on_mobile = fa
             <div className={[navbar.header_logo_button, logoPositionClass].join(" ")}>
                 <Link href="/" className={[navbar.header_link, logoPositionClass].join(" ")}>
                     <div className={[navbar.header_logo_div, logoPositionClass].join(" ")}>
-                        <div className={navbar.kssdir_logo_BG}>
+                        <div className={navbar.dir_logo_BG}>
                             <img src = "/svg_assets/compass_logo_vector.svg" className={navbar.kssdir_logo}/>
                         </div>
-                        <p style={{color:text_color}} className={navbar.kssdir_logo_text}>DIRECTORY</p>                        
+                        <p style={{color:text_color}} className = {navbar.kssdir_logo_text}>DIRECTORY</p>                        
                     </div>
+                </Link>
+            </div>
+            <div className = {navbar.headerOptionsDiv}>
+                <Link href = "/" className = {navbar.headerOption}>
+                    <p>hi</p>
+                </Link>
+                <Link href = "/" className = {navbar.headerOption}>
+                    <p>boo</p>
                 </Link>
             </div>
             {extra_additions} 
