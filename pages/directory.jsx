adb3c5a185372ee5c0e38d89cfd9c866c75bc5e8
Tyishen <ethant.ca@gmail.com>
2024-09-13 22:59:27 -0400

    hero + homepage functional!!!

--- pages/directory.jsx
+++ pages/directory.jsx
@@ -36,29 +36,33 @@ function homepage() {
                                 <rect className = {main.gradient1}/>
                                 <div className = {main.heroContent_div}>
                                     <h1 className = {main.heroText}>supporting <br/>student-made <br/>services</h1>
-                                    <div id = {main.starLogo}>
-                                        <img src = "svg_assets/logo_assets/innerStar.svg" id = {main.innerStar}/>
-                                        <svg id = {main.outerStarSVGBox1}>
-                                        <path className = {main.outerStar} stroke="white" fill = "none" stroke-width="2.2" d="m90.02,0l-.58,14.41-1.44,36.03-1.28,31.91-1.47-.72-11.53-5.64-7.69-3.76,5.2,6.8,7.8,10.2,1.02,1.33-30.98,7.51-35.05,8.5-14.02,3.4,14.41.58,36.03,1.44,31.91,1.28-.72,1.47-5.64,11.53-3.76,7.69,6.8-5.2,10.2-7.8,1.33-1.02,7.51,30.98,8.5,35.05,3.4,14.02.58-14.41,1.44-36.03,1.28-31.91,1.47.72,11.53,5.64,7.69,3.76-5.2-6.8-7.8-10.2-1.02-1.33,30.98-7.51,35.05-8.5,14.02-3.4-14.41-.58-36.03-1.44-31.91-1.28.72-1.47,5.64-11.53,3.76-7.69-6.8,5.2-10.2,7.8-1.33,1.02-7.51-30.98-8.5-35.05-3.4-14.02h0Z"/>
-                                        </svg>
-                                        <svg id = {main.outerStarSVGBox2}>
-                                        <path className = {main.outerStar} stroke="white" fill = "none" stroke-width="2.2" d="m90.02,0l-.58,14.41-1.44,36.03-1.28,31.91-1.47-.72-11.53-5.64-7.69-3.76,5.2,6.8,7.8,10.2,1.02,1.33-30.98,7.51-35.05,8.5-14.02,3.4,14.41.58,36.03,1.44,31.91,1.28-.72,1.47-5.64,11.53-3.76,7.69,6.8-5.2,10.2-7.8,1.33-1.02,7.51,30.98,8.5,35.05,3.4,14.02.58-14.41,1.44-36.03,1.28-31.91,1.47.72,11.53,5.64,7.69,3.76-5.2-6.8-7.8-10.2-1.02-1.33,30.98-7.51,35.05-8.5,14.02-3.4-14.41-.58-36.03-1.44-31.91-1.28.72-1.47,5.64-11.53,3.76-7.69-6.8,5.2-10.2,7.8-1.33,1.02-7.51-30.98-8.5-35.05-3.4-14.02h0Z"/>
-                                        </svg>
-                                        <div id = {main.ring1}>
-                                        <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing1}/>
-                                        <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing1}/>
+                                    <div className = {main.heroLogoDiv}>
+                                        <div id = {main.starLogo}>
+                                            <img src = "svg_assets/logo_assets/innerStar.svg" id = {main.innerStar}/>
+                                            <svg id = {main.outerStarSVGBox1}>
+                                            <path className = {main.outerStar} stroke="white" fill = "none" stroke-width="2.2" d="m90.02,0l-.58,14.41-1.44,36.03-1.28,31.91-1.47-.72-11.53-5.64-7.69-3.76,5.2,6.8,7.8,10.2,1.02,1.33-30.98,7.51-35.05,8.5-14.02,3.4,14.41.58,36.03,1.44,31.91,1.28-.72,1.47-5.64,11.53-3.76,7.69,6.8-5.2,10.2-7.8,1.33-1.02,7.51,30.98,8.5,35.05,3.4,14.02.58-14.41,1.44-36.03,1.28-31.91,1.47.72,11.53,5.64,7.69,3.76-5.2-6.8-7.8-10.2-1.02-1.33,30.98-7.51,35.05-8.5,14.02-3.4-14.41-.58-36.03-1.44-31.91-1.28.72-1.47,5.64-11.53,3.76-7.69-6.8,5.2-10.2,7.8-1.33,1.02-7.51-30.98-8.5-35.05-3.4-14.02h0Z"/>
+                                            </svg>
+                                            <svg id = {main.outerStarSVGBox2}>
+                                            <path className = {main.outerStar} stroke="white" fill = "none" stroke-width="2.2" d="m90.02,0l-.58,14.41-1.44,36.03-1.28,31.91-1.47-.72-11.53-5.64-7.69-3.76,5.2,6.8,7.8,10.2,1.02,1.33-30.98,7.51-35.05,8.5-14.02,3.4,14.41.58,36.03,1.44,31.91,1.28-.72,1.47-5.64,11.53-3.76,7.69,6.8-5.2,10.2-7.8,1.33-1.02,7.51,30.98,8.5,35.05,3.4,14.02.58-14.41,1.44-36.03,1.28-31.91,1.47.72,11.53,5.64,7.69,3.76-5.2-6.8-7.8-10.2-1.02-1.33,30.98-7.51,35.05-8.5,14.02-3.4-14.41-.58-36.03-1.44-31.91-1.28.72-1.47,5.64-11.53,3.76-7.69-6.8,5.2-10.2,7.8-1.33,1.02-7.51-30.98-8.5-35.05-3.4-14.02h0Z"/>
+                                            </svg>
+                                            <div id = {main.ring1}>
+                                            <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing1}/>
+                                            <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing1}/>
+                                            </div>
+                                            <div id = {main.ring2}>
+                                            <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing2}/>
+                                            <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing2}/>
+                                            </div>
+                                            <div id = {main.ring3}>
+                                            <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing3}/>
+                                            <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing3}/>
+                                            </div>
+                                            <div id = {main.ring4}>
+                                            <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing4}/>
+                                            <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing4}/>
+                                            </div>
                                         </div>
-                                        <div id = {main.ring2}>
-                                        <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing2}/>
-                                        <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing2}/>
-                                        </div>
-                                        <div id = {main.ring3}>
-                                        <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing3}/>
-                                        <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing3}/>
-                                        </div>
-                                        <div id = {main.ring4}>
-                                        <img src = "svg_assets/logo_assets/innerRing.svg" id = {main.innerRing4}/>
-                                        <img src = "svg_assets/logo_assets/outerRing.svg" id = {main.outerRing4}/>
+                                        <div className = {main.logoBackground}>
                                         </div>
                                     </div>
                                 </div>
