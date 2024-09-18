8d6e46ad3fe9d3fc191032e2c17aec1e56a8de61
Toast-Bucket <71938947+Kabinet0@users.noreply.github.com>
2024-09-18 17:51:15 -0400

    Sort active clubs before inactive ones

--- components/ClubRepoButtonList.jsx
+++ components/ClubRepoButtonList.jsx
@@ -9,13 +9,23 @@ function ClubRepoButtonList({categoryClubData, categoryColor = "#6485D8"}) {
         return webServerURL + "/specific_club_images/" + URL + "/banner?hash=" + image_hash;
     }
 
-
-    let clubs = [];
+    let activeClubs = [];
+    let inactiveClubs = [];
     for (const i in categoryClubData) {
         if (categoryClubData[i].Listed === "Yes") {
-            clubs.push(categoryClubData[i]);
+            if (categoryClubData[i].Activity == "Yes") {
+                activeClubs.push(categoryClubData[i]);
+            } else {
+                inactiveClubs.push(categoryClubData[i]);
+            }
         }
     }
+
+    let clubs = [];
+    clubs.push(...activeClubs);
+    clubs.push(...inactiveClubs);
+
+    //console.log("Clubs " + JSON.stringify(clubs, null, 4));
 
     const clubButtons = clubs.map(club => {
         const bannerURL = "banner" in club["Images"] ?  bannerGetRequest(club["URL"], club["Images"]["banner"]["Hash"]) : "";
