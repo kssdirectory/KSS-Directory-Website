5c0c2be493b19f777dd76b2662144750bd078a6a
Tyishen <ethant.ca@gmail.com>
2024-09-21 14:24:52 -0400

    more funnies

--- components/CafeteriaBox.jsx
+++ components/CafeteriaBox.jsx
@@ -3,7 +3,7 @@ import caf from "../styles/cafeteria-box.module.css"
 
 const webServerURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-function cafeteriaBoxElement() {
+function cafeteriaMenu() {
 
     const loading = (
         <div className = {caf.cafMenuBox}>
@@ -63,4 +63,4 @@ function cafeteriaBoxElement() {
     )
  };
 
- export default cafeteriaBoxElement;
+ export default cafeteriaMenu;
